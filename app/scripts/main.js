c5cc1732fce0190e21f868cda5453137b83b941c
Brandon Miller <brandonmiller@unknown84383564ec6c.gateway.2wire.net>
2013-09-08 22:10:59 -0400

    First Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,94 @@
+function MeasurementLength (option) {
+	option = option || {}
+	this.inches = option.inches || 0
+	this.feet = option.feet || 0
+	this.yards = option.yards || 0
+	this.centimeters = option.centimeters || 0
+	this.meters = option.meters || 0
+
+
+}
+
+
+  $('.btn-success').click(function(){
+    getFormValues()
+    fillObject (input)
+    conversionListUpdate (input)
+    $('input-inches').val(input.inches)
+	$('input-feet').val(input.feet)
+	$('input-yards').val(input.yards)
+  })
+
+
+
+function getFormValues () {
+  var inches = $('.inches-input').val();
+  var feet  = $('.feet-input').val();
+  var yards  = $('.yards-input').val();
+  var centimeters = $('.centimeters-input').val();
+  var meters = $('.meters-input').val();
+  input  = new MeasurementLength 
+    input.inches = inches
+    input.feet = feet
+    input.yards = yards
+    input.centimeters = centimeters
+    input.meters= meters
+
+  return input
+}
+
+
+function fillObject (object) {
+
+	if (object.inches > 0){
+		object.feet= object.inches/12
+		object.yards=object.inches/36
+		object.centimeters= object.inches*2.54
+		object.meters = object.centimeters/100
+		
+	}
+
+	else  if (object.feet > 0){
+		object.inches = object.feet*12
+		object.yards = object.feet/3
+		object.centimeters = object.inches * 2.54
+		object.meters = object.centimeters/100
+	}
+
+	else if (object.yards > 0 ){
+		object.inches = object.yards * 36
+		object.feet = object.yards * 3
+		object.centimeters = object.inches * 2.54
+		object.meters = object.centimeters/100
+	}
+
+	else if (object.centimeters > 0) {
+		object.inches = object.centimeters/2.54
+		object.feet= object.inches/12
+		object.yards=object.inches/36
+		object.meters = object.centimeters/100
+		
+	}
+
+	else if (object.meters > 0) {
+		object.centimeters= object.meters * 100
+		object.inches = object.centimeters/2.54
+		object.feet= object.inches/12
+		object.yards=object.inches/36
+	}
+	return object
+
+
+
+}
+
+function conversionListUpdate (object) {
+  var ul = $('.conversion-list ul');
+  ul.html('');
+
+  
+    var text = "<li>"+ object.inches + " inches </li> <li>" + object.feet + " feet </li> <li> " + object.yards + " yards </li> <li> " + object.centimeters + " centimeters </li> <li>" + object.meters + " meters </li>";
+    ul.append(text);
+  
+}
+
---
+++ test/spec/test.js
@@ -0,0 +1,148 @@
+/*global describe, it */
+'use strict';
+// (function () {
+//     describe('Give it some context', function () {
+//         describe('maybe a bit more context here', function () {
+//             it('should run here few assertions', function () {
+
+//             });
+//         });
+//     });
+// })();
+(function() {
+	describe('Conversion App', function () {
+		describe('should have set default values of inputs to zero', function() {
+			var input = new MeasurementLength ();
+	
+			it("such as the value for inches", function (){
+				expect(input.inches).equals(0)
+			})
+			it("such as the value for feet", function (){
+				expect(input.feet).equals(0)
+			})
+			it("such as the value for yards", function (){
+				expect(input.yards).equals(0)
+			})
+			it("such as the value for centimeters", function (){
+				expect(input.centimeters).equals(0)
+			})
+			it("such as the value for meters", function (){
+				expect(input.meters).equals(0)
+			})
+		})
+
+		describe ('should allow for the alteration of values when offered', function(){
+			var input = new MeasurementLength ();
+			it('such as setting inches to 10', function(){
+				input.inches=10;
+				expect(input.inches).equals(10)
+			})
+			it('such as setting feet to 5', function(){
+				input.feet=5;
+				expect(input.feet).equals(5)
+			})
+			it('such as setting yards to 20', function(){
+				input.yards=20;
+				expect(input.yards).equals(20)
+			})
+			it('such as setting centimeters to 2', function(){
+				input.centimeters=2;
+				expect(input.centimeters).equals(2)
+			})
+			it('such as setting meters to 8', function(){
+				input.meters=8;
+				expect(input.meters).equals(8)
+			})
+
+		})
+
+		describe ('when given a value of 36 for inches it should convert it to', function(){
+			var input = new MeasurementLength ();
+			input.inches = 36;
+			fillObject(input);
+			it('3 feet', function(){
+				expect(input.feet).equals(3)
+			})
+			it('1 yard', function(){
+				expect(input.yards).equals(1)
+			})
+			it('91.44 centimeters', function(){
+				expect(input.centimeters).equals(91.44)
+			})
+			it('0.9144 yards', function(){
+				expect(input.meters).equals(0.9144)
+			})
+		})
+		describe ('when given a value of 3 for feet it should convert it to', function(){
+			var input = new MeasurementLength ();
+			input.feet = 3;
+			fillObject(input);
+			it('36 inches', function(){
+				expect(input.inches).equals(36)
+			})
+			it('1 yard', function(){
+				expect(input.yards).equals(1)
+			})
+			it('91.44 centimeters', function(){
+				expect(input.centimeters).equals(91.44)
+			})
+			it('0.9144 meters', function(){
+				expect(input.meters).equals(0.9144)
+			})
+		})
+		describe ('when given a value of 1 for yards it should convert it to', function(){
+			var input = new MeasurementLength ();
+			input.yards = 1;
+			fillObject(input);
+			it('36 inches', function(){
+				expect(input.inches).equals(36)
+			})
+			it('3 feet', function(){
+				expect(input.feet).equals(3)
+			})
+			it('91.44 centimeters', function(){
+				expect(input.centimeters).equals(91.44)
+			})
+			it('0.9144 meters', function(){
+				expect(input.meters).equals(0.9144)
+			})
+		})
+		describe ('when given a value of 91.44 for centimeters it should convert it to', function(){
+			var input = new MeasurementLength ();
+			input.centimeters = 91.44;
+			fillObject(input);
+			it('36 inches', function(){
+				expect(input.inches).equals(36)
+			})
+			it('3 feet', function(){
+				expect(input.feet).equals(3)
+			})
+			it('1 yard', function(){
+				expect(input.yards).equals(1)
+			})
+			it('0.9144 meters', function(){
+				expect(input.meters).equals(0.9144)
+			})
+		})
+
+		describe ('when given a value of .9144 for meters it should convert it to', function(){
+			var input = new MeasurementLength ();
+			input.meters = 0.9144;
+			fillObject(input);
+			it('36 inches', function(){
+				expect(input.inches).equals(36)
+			})
+			it('3 feet', function(){
+				expect(input.feet).equals(3)
+			})
+			it('1 yard', function(){
+				expect(input.yards).equals(1)
+			})
+			it('91.44 centimeters', function(){
+				expect(input.centimeters).equals(91.44)
+			})
+		})
+
+	})
+
+}) ();
